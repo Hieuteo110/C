fec641faa9f23ff6d916bae2ef02cb357f1ad35b
Hieuteo110 <97657975+Hieuteo110@users.noreply.github.com>
2025-10-23 07:54:08 +0700

    Update 3.6.cpp

--- 3.6.cpp
+++ 3.6.cpp
@@ -1,18 +1,22 @@
 #include <stdio.h>
 int main ()
 {
-	int n,i=1,s=0,s1;
+	int n,i=1,s=0,j,s1;
 	printf("nhap vao so nguyen n = ");
 	scanf("%d",&n);
-	while (i<=n)
+	while(n<0||n>10)
 	{
+	printf("nhap lai so nguyen n = ");
+	scanf("%d",&n);	
+	}
+	while (i<=n)
+{
 		s1=1;
-		for(int j=1;j<=i;j++)
+		for(j=1;j<=i;j++)
 			s1=s1*j;
 		s=s+s1;
 		i++;
-	}
+}
 printf("tong s = %d",s);
 return 0;
 }
-
