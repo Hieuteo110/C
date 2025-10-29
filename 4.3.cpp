b9be9b048f1da4fbe824222048338d86f6ccd0af
Hieuteo110 <97657975+Hieuteo110@users.noreply.github.com>
2025-10-29 16:16:33 +0700

    Update 4.3.cpp

--- 4.3.cpp
+++ 4.3.cpp
@@ -7,15 +7,8 @@ int main()
 		scanf("%d",&n);
 	if(n<0) goto nhap;
 	for (int i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			s+=i;
-		}
-	}
-	if(n*2==s) printf("%d la so hoan thien");
-	else printf("%d khong phai la so hoan thien");
-
+		if(n%i==0) s+=i;
+	if(n*2==s) printf("%d la so hoan thien",n);
+	else printf("%d khong phai la so hoan thien",n);
 return 0;
 }
-
