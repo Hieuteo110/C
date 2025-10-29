936caf3d3e9cbef641a8ca4ae1ceba93aa596ad1
Hieuteo110 <97657975+Hieuteo110@users.noreply.github.com>
2025-10-29 20:51:52 +0700

    Create 4.5.cpp

---
+++ 4.5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	nhaplai:
+		printf("Nhap so nguyen duong n: ");
+		scanf("%d",&n);
+	if (n<0) goto nhaplai;
+	int a[n];
+	a[0]=1;
+	a[1]=1;
+	for (int i=2;i<n;i++)
+		a[i]=a[i-2]+a[i-1];
+	printf("So fibonaxi thu %d = %d",n,a[n-1]);
+return 0;
+}
