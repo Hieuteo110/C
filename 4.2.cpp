db1b8a7cc851f6d01f2bedec32f598e24d56be7f
Hieuteo110 <97657975+Hieuteo110@users.noreply.github.com>
2025-10-29 16:08:04 +0700

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -7,14 +7,9 @@ int main()
 		scanf("%d",&n);
 	if(n<0) goto nhap;
 	for (int i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			s+=i;
-		}
-	}
+		if(n%i==0) s+=i;
 	printf("Tong cac uoc cua %d: %d",n,s);
-
 return 0;
 }
+
 
